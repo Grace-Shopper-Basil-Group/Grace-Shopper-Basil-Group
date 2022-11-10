690fb50980ff366c2e0fcf6f9d3ba2bb339a3a20
Anthony Zhang <zhang.anthony97@gmail.com>
2022-11-10 11:29:47 -0500

    feat(AllProducts view): added basic rendering

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,14 +3,35 @@ import { connect } from "react-redux";
 import { fetchProducts } from "../store/allProducts";
 
 export class AllProducts extends Component {
+  componentDidMount() {
+    this.props.getProducts();
+  }
   render() {
-    return <div> Test</div>;
+    const allProducts = this.props.allProducts;
+    console.log("allProducts", allProducts);
+    return (
+      <div>
+        <h2>Products</h2>
+        <ul>
+          {allProducts.map((product) => (
+            <div key={product.id}>
+              <img src={product.imageUrl} alt="product image" />
+              <br></br>
+              {product.name}
+              <br></br>
+              {product.description}
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log("state", state);
   return {
-    products: state.products,
+    allProducts: state.allProducts,
   };
 };
 
